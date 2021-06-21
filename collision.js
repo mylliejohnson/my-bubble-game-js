8ec3e504acc24979cd7105a1aa1aeac9f4cbe13d
Blake Stillwell <bstillwell13@yahoo.com>
2021-06-21 15:33:06 -0400

    collisions added

---
+++ collision.js
@@ -0,0 +1,25 @@
+// CIRCLE/RECTANGLE
+function circleRect(cx, cy, radius, rx, ry, rw, rh) {
+  // temporary variables to set edges for testing
+  testX = cx;
+  testY = cy;
+
+  // which edge is closest?
+  if (cx < rx) testX = rx;
+  // test left edge
+  else if (cx > rx + rw) testX = rx + rw; // right edge
+  if (cy < ry) testY = ry;
+  // top edge
+  else if (cy > ry + rh) testY = ry + rh; // bottom edge
+
+  // get distance from closest edges
+  distX = cx - testX;
+  distY = cy - testY;
+  distance = Math.sqrt(distX * distX + distY * distY);
+
+  // if the distance is less than the radius, collision!
+  if (distance <= radius) {
+    return true;
+  }
+  return false;
+}
--- pins-down.js
+++ pins-down.js
@@ -1,29 +1,28 @@
+// class downPin {
+//     constructor(x,y,w,h,color){
+//        this.x = x
+//        this.y = y
+//        this.w = w
+//        this.h = h
+//        this.color = color
+//    }
 
-class downPin {
-    constructor(x,y,w,h,color){
-       this.x = x
-       this.y = y
-       this.w = w
-       this.h = h
-       this.color = color
-   }
+//    draw = () => {
+//        ctx.fillStyle = this.color
+//        ctx.fillRect(this.x, this.y, this.w, this.h)
+//    }
 
-   draw = () => {
-       ctx.fillStyle = this.color
-       ctx.fillRect(this.x, this.y, this.w, this.h)
-   }
+//    move = () => {
+//        this.y += 10
+//        this.draw()
+//    }
+// }
 
-   move = () => {
-       this.y += 10
-       this.draw()
-   }
-}
+// let verticalPins = []
 
-let verticalPins = []
+// setInterval(() => {
+//     let pinsDown = new downPin(
+//         Math.max(Math.random() * 800), 0, 10, 75, "grey");
+//         verticalPins.push(pinsDown)
 
-setInterval(() => {
-    let pinsDown = new downPin(
-        Math.max(Math.random() * 800), 0, 10, 75, "grey");
-        verticalPins.push(pinsDown)
-   
-}, Math.random() * 7000);
+// }, Math.random() * 7000);
