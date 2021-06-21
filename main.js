8ec2364f0962a38896f399eea32b226dd5bc3b4e
Mahim Pritom <mahimnyc@yahoo.com>
2021-06-21 15:46:45 -0400

    intro page

--- main.js
+++ main.js
@@ -8,88 +8,91 @@
 // create a start intro ? maybe
 // animateId = null ??
 
-
 // canvas setup
-const canvas = document.querySelector('canvas')
-const ctx = canvas.getContext('2d')
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
 
 // create bubble
 class Bubble {
-    constructor(x, y, r, sAngle, eAngle){
-        this.x = x
-        this.y = y
-        this.r = r
-        this.sAngle = sAngle
-        this.eAngle = eAngle
-    }
+  constructor(x, y, r, sAngle, eAngle) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.sAngle = sAngle;
+    this.eAngle = eAngle;
+  }
 
-    draw = () => {
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
-        ctx.fillStyle = '#A9D0F5';
-        ctx.fill();
-    }
+  draw = () => {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+    ctx.fillStyle = "#A9D0F5";
+    ctx.fill();
+  };
 }
 
 // the bubble
-let bubb = new Bubble(canvas.width/2, canvas.height/2, 10, 0, 1*Math.PI);
+let bubb = new Bubble(canvas.width / 2, canvas.height / 2, 10, 0, 1 * Math.PI);
 
 // key functions
-window.onkeydown = function(e){
-    if(e.key === " " && bubb.r < 175){
-        bubb.r++
-    }
-}
+window.onkeydown = function (e) {
+  if (e.key === " " && bubb.r < 175) {
+    bubb.r++;
+  }
+};
 
-let score = 0; 
-window.onkeyup = function(e){
-    switch (e.key === " "){
-        case bubb.r < 50: 
-            score += 25
-            break;
-        case bubb.r < 100:
-            score += 75
-            break;
-        case bubb.r < 150:
-            score += 100
-            break;
-        case bubb.r > 150:
-            score += 200            
-            break;
-    }
-    bubb.r = 10
-}
+let score = 0;
+window.onkeyup = function (e) {
+  switch (e.key === " ") {
+    case bubb.r < 50:
+      score += 25;
+      break;
+    case bubb.r < 100:
+      score += 75;
+      break;
+    case bubb.r < 150:
+      score += 100;
+      break;
+    case bubb.r > 150:
+      score += 200;
+      break;
+  }
+  bubb.r = 10;
+};
 
 // create pins
 class Pin {
-     constructor(x,y,w,h,color){
-        this.x = x
-        this.y = y
-        this.w = w
-        this.h = h
-        this.color = color
-    }
+  constructor(x, y, w, h, color) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.color = color;
+  }
 
-    draw = () => {
-        ctx.fillStyle = this.color
-        ctx.fillRect(this.x, this.y, this.w, this.h)
-    }
+  draw = () => {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.w, this.h);
+  };
 
-    move = () => {
-        this.x -= 25
-        this.draw()
-    }
+  move = () => {
+    this.x -= 25;
+    this.draw();
+  };
 }
 
-// interval for pins 
+// interval for pins
 // give min and max y values to be in range with the bubble **
-const pinPops = []
+const pinPops = [];
 
 setInterval(() => {
-    let pins = new Pin(
-        canvas.width, Math.max(Math.random() * 500), 75, 10, "grey");
-        pinPops.push(pins)
-   
+  let pins = new Pin(
+    canvas.width,
+    Math.max(Math.random() * 500),
+    75,
+    10,
+    "grey"
+  );
+  pinPops.push(pins);
 }, Math.random() * 5500);
 
 // lives
@@ -115,29 +118,27 @@ setInterval(() => {
 //     }
 // }
 
-
-
 // animate it!
 let animateId = null;
 
-function animate(){
-    animateId = requestAnimationFrame(animate)
-    ctx.clearRect(0,0,canvas.width, canvas.height)
-    
-    bubb.draw()
+function animate() {
+  animateId = requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    ctx.fillText(score, 20, 50)
+  bubb.draw();
 
-    // when collision is detected, pop a life off of the array 
-    ctx.fillText("XXX", canvas.width - 130, canvas.height - 25) // turn into array to pop off when life is lost!!!
+  ctx.fillText(score, 20, 50);
 
-    for(let pins of pinPops){
-        pins.move();
-    }
+  // when collision is detected, pop a life off of the array
+  ctx.fillText("XXX", canvas.width - 130, canvas.height - 25); // turn into array to pop off when life is lost!!!
 
-    let life = "X"
+  for (let pins of pinPops) {
+    pins.move();
+  }
+
+  let life = "X";
 }
 
-ctx.font = "48px monospace"
+ctx.font = "48px monospace";
 
-animate()
+animate();
