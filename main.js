1cea33797c57276c4a78616f5dd335bc7ec3d431
leannewilson <83836254+leannewilson@users.noreply.github.com>
2021-06-25 10:10:29 -0700

    stop lives from falling at end of game

--- main.js
+++ main.js
@@ -112,7 +112,7 @@ function displayLives() {
   }
 
   if (lives.length == 0) {
-    cancelAnimationFrame(animateId);
+    // cancelAnimationFrame(animateId);
     console.log("game over");
     bgMusic.pause();
   }
@@ -319,24 +319,24 @@ function animate() {
     ctx.font = "48px Teko, san-serif";
 
     //Review Animation
-    document.querySelector('.ml15').style.display = "block"
-    anime
-    .timeline({ loop: true })
-    .add({
-      targets: ".ml15 .word",
-      scale: [14, 1],
-      opacity: [0, 1],
-      easing: "easeOutCirc",
-      duration: 800,
-      delay: (el, i) => 800 * i,
-    })
-    .add({
-      targets: ".ml15",
-      opacity: 0,
-      duration: 1000,
-      easing: "easeOutExpo",
-      delay: 1000,
-    });
+    // document.querySelector('.ml15').style.display = "block"
+    // anime
+    // .timeline({ loop: true })
+    // .add({
+    //   targets: ".ml15 .word",
+    //   scale: [14, 1],
+    //   opacity: [0, 1],
+    //   easing: "easeOutCirc",
+    //   duration: 800,
+    //   delay: (el, i) => 800 * i,
+    // })
+    // .add({
+    //   targets: ".ml15",
+    //   opacity: 0,
+    //   duration: 1000,
+    //   easing: "easeOutExpo",
+    //   delay: 1000,
+    // });
   }
 
   // --- SCORE KEEPING --- //
